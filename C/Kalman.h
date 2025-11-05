c85e44cb4a5f41bed472a58d109d86b3cc5dbeef
Ali Yağız Uz <aliyagizuz@gmail.com>
2025-11-06 00:37:35 +0300

    -main.py,  kalman.h ve kalman.c  dosyaları eklendi. -Kalman dosyalarında ilk kodlar eklendi

---
+++ C/Kalman.h
@@ -0,0 +1,28 @@
+  /* ----------------------------------------------- */
+ /* kalmanın kullandığı fonksiyonlar burda tanımlı  */
+/* ----------------------------------------------- */ 
+#ifndef INC_KALMAN_H_
+#define INC_KALMAN_H_
+
+#include<stdio.h>
+
+typedef struct Kalman
+{
+    /* data */
+};
+
+
+
+
+void KalmanFilter();
+
+
+
+
+
+
+
+
+
+
+#endif 
--- C/kalman.c
+++ C/kalman.c
@@ -1,2 +1,9 @@
+/*
+ * kalman.c
+ *
+ *  Created on: Nov 5, 2025
+ *      Author: aliya
+ */
+
 #include "kalman.h"
 
